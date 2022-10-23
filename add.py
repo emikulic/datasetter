a614d6a97849018924d2ccdd26a6d561f94d5053
Emil Mikulic <emikulic@gmail.com>
2022-10-23 18:00:55 +1100

    add: next_n and walk_dir (#3)
    * add: implement next_n.
    
    * add: implement walk_dir.

--- add.py
+++ add.py
@@ -5,12 +5,15 @@ Add images to a dataset.
 from PIL import Image, ImageFile
 import argparse
 import json
+import os
 
 
 class Dataset:
     def __init__(self, fn):
-        self.data = {}  # Map from N to metadata object.
-        self._load(fn)
+        self._data = {}  # Map from N to metadata object.
+        self._fn = fn
+        if os.path.exists(fn):
+            self._load(fn)
 
     def _load(self, fn):
         """
@@ -21,7 +24,24 @@ class Dataset:
                 obj = json.loads(line)
                 assert "n" in obj, obj
                 n = int(obj["n"])
-                self.data[n] = obj
+                self._data[n] = obj
+
+    def next_n(self):
+        """
+        Returns the next N.
+        """
+        return max([-1] + list(self._data.keys())) + 1
+
+
+def walk_dir(path):
+    """
+    Recursively walk the given path and return a list of filenames.
+    """
+    out = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        for fn in filenames:
+            out.append(f"{dirpath}/{fn}")
+    return out
 
 
 def main():
@@ -32,3 +52,20 @@ def main():
     args = p.parse_args()
 
     ds = Dataset(args.dsfile)
+
+    fns = []
+    for i in args.inputs:
+        if os.path.isdir(i):
+            fns += walk_dir(i)
+        else:
+            assert os.path.isfile(i), i
+            fns.append(i)
+
+    print(fns)
+    # print(args.inputs)
+    # print(ds._data)
+    # print(ds.next_n())
+
+
+if __name__ == "__main__":
+    main()
