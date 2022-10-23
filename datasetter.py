a8b187451ff41dd147708d261c3ac966dd20f0bd
Emil Mikulic <emikulic@gmail.com>
2022-10-23 14:34:08 +1100

    Implement thumbnails.

--- datasetter.py
+++ datasetter.py
@@ -2,24 +2,29 @@
 """
 Web-based dataset editing.
 """
-# from PIL import Image, ImageFile
+from PIL import Image, ImageFile
 import argparse
 import os
 from util import Dataset
 from aiohttp import web
+from prep import rgbify  # TODO: move to util.py
 
 # import urllib.parse
 import aiohttp
 import logging
+from io import BytesIO
 
-logging.basicConfig(level=logging.DEBUG)
+# Don't throw exception when a file only partially loads.
+ImageFile.LOAD_TRUNCATED_IMAGES = True  # TODO: factor out loading into util.py
+
+logging.basicConfig(level=logging.INFO)
 
 
 routes = web.RouteTableDef()
 
 
 @routes.get("/")
-async def index(request):
+async def index_html(request):
     with open("index.html", "r") as f:
         s = f.read()
     return web.Response(
@@ -28,7 +33,7 @@ async def index(request):
 
 
 @routes.get("/jquery.js")
-async def index(request):
+async def jquery_js(request):
     with open("jquery.js", "r") as f:
         s = f.read()
     return web.Response(text=s, content_type="text/javascript")
@@ -42,16 +47,35 @@ async def title(request):
 
 
 @routes.get("/data.json")
-async def title(request):
+async def data(request):
     return web.json_response(
         request.config_dict["ds"]._data, headers={"Pragma": "no-cache"}
     )
+
+
+@routes.get("/thumbnail/{sz}/{n}")
+async def thumbnail(request):
+    n = int(request.match_info.get("n", ""))
+    sz = int(request.match_info.get("sz", ""))
+    o = request.config_dict["ds"]._data[n]
+    # TODO: factor out loading into util.py:
+    img = Image.open(o["fn"])
+    # TODO: exif rotation
+    img = Image.fromarray(rgbify(img))
+    x, y, w, h = o["x"], o["y"], o["w"], o["h"]
+    img = img.crop((x, y, x + w, y + h))
+    img = img.resize((sz, sz), Image.Resampling.BICUBIC)
+    s = BytesIO()
+    img.save(s, format="jpeg", quality=95)
+    # TODO: caching
+    return web.Response(body=s.getvalue(), content_type="image/jpeg")
 
 
 def main():
     p = argparse.ArgumentParser()
     p.add_argument("--port", type=int, default=8001)
     p.add_argument("--host", type=str, default="127.0.0.1", help="Bind address.")
+    # p.add_argument("--size", type=int, default=512, help="Default output image size.")
     p.add_argument("dsfile", help="JSON dataset file to operate on.")
     args = p.parse_args()
 
