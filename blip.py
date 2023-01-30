a2f195e7a510b5b22442f91073a3969c9da35f92
Emil Mikulic <emikulic@gmail.com>
2023-01-30 18:18:34 +1100

    blip.py: autoformat.

--- blip.py
+++ blip.py
@@ -2,7 +2,7 @@
 """
 Use BLIP to automatically add captions.
 """
-print('importing')
+print("importing")
 import argparse
 from util import Dataset
 from PIL import Image
@@ -13,39 +13,44 @@ from transformers import AutoProcessor, BlipForConditionalGeneration
 # Trade-off: use default size to get cache hits, BLIP will scale images down to 384px.
 SZ = 512
 
+
 def main():
     p = argparse.ArgumentParser()
-    p.add_argument("--prefix", default='', help="Optional prefix.")
+    p.add_argument("--prefix", default="", help="Optional prefix.")
     p.add_argument("dsfile", help="JSON dataset file to add to.")
     args = p.parse_args()
 
-    print('loading dataset')
+    print("loading dataset")
     ds = Dataset(args.dsfile)
 
     # Downloads 945MB to ~/.cache/huggingface/hub/models--Salesforce--blip-image...
-    print('loading processor')
+    print("loading processor")
     processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-    print('loading model')
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+    print("loading model")
+    model = BlipForConditionalGeneration.from_pretrained(
+        "Salesforce/blip-image-captioning-base"
+    )
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
-    print('done loading')
+    print("done loading")
 
     for n, md in ds._data.items():
-        if 'autocaption' in md:
-            print('skipping', md)
+        if "autocaption" in md:
+            print("skipping", md)
             continue
         jpg = ds.cropped_jpg(n, SZ)
         im = Image.open(io.BytesIO(jpg))
 
-        with torch.no_grad(): # matters
+        with torch.no_grad():  # matters
             inputs = processor(images=im, return_tensors="pt").to(device)
-            outputs = model.generate(**inputs, max_new_tokens=80)#, num_return_sequences=10, do_sample=True)
+            outputs = model.generate(
+                **inputs, max_new_tokens=80
+            )  # , num_return_sequences=10, do_sample=True)
             caption = processor.decode(outputs[0], skip_special_tokens=True)
 
-        md['autocaption'] = args.prefix + caption
+        md["autocaption"] = args.prefix + caption
         ds.add(md)
-        print((n, md['fn'], md['autocaption']))
+        print((n, md["fn"], md["autocaption"]))
 
 
 if __name__ == "__main__":
--- format.sh
+++ format.sh
@@ -5,5 +5,5 @@
 # pip install black
 # sudo apt install clang-format
 set -x
-black add.py util.py prep.py datasetter.py autocaption/autocaption.py
+black add.py util.py prep.py datasetter.py blip.py
 clang-format-10 -i index.js
