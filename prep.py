21fe663055f2aca31b2746d62cecd73161b89769
Emil Mikulic <emikulic@gmail.com>
2023-02-08 01:57:49 +1100

    prep.py: add --limit flag to reduce outputs.

--- prep.py
+++ prep.py
@@ -12,6 +12,7 @@ def main():
     p.add_argument("outdir", help="Dataset directory to generate.")
     p.add_argument("inputs", nargs="+", help="One or more dataset JSON files.")
     p.add_argument("--size", type=int, default=512, help="Default output image size.")
+    p.add_argument("--limit", type=int, default=0, help="Stop after this many inputs.")
     p.add_argument(
         "--need_crop",
         help="If set, skip any input that isn't manually cropped.",
@@ -33,6 +34,7 @@ def main():
     print(f"loaded {dsn} datasets")
 
     # Process all inputs.
+    count = 0
     for dsi, ds in enumerate(datasets):
         on = len(ds._data)
         for oi, o in enumerate(ds._data.values()):
@@ -56,6 +58,11 @@ def main():
             else:
                 print(f'skip {o["fn"]} missing caption and autocaption')
                 continue
+
+            # Stop at limit.
+            count += 1
+            if args.limit > 0 and count > args.limit:
+                return
 
             # Write img/ and txt/.
             img = ds.cropped_jpg(o["n"], args.size)
