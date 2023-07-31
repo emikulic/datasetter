1dbb434f7bd94f1208b45aefddd518ec429cd396
Emil Mikulic <emikulic@gmail.com>
2023-07-31 19:43:57 +1000

    Add re_pad script.
    Like `add --pad` retroactively.

---
+++ re_pad.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Pad all images.
+"""
+import argparse
+import os
+import hashlib
+import util
+import PIL
+from add import pad_to_square, md5
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument("dsfile", help="JSON dataset to process.")
+    args = p.parse_args()
+
+    # Check inputs are relative to the dsfile.
+    dsdir = os.path.dirname(os.path.abspath(args.dsfile))
+    if dsdir == "":
+        dsdir = "."
+    print(f"working relative to {dsdir!r}")
+
+    # Load dataset.
+    ds = util.Dataset(args.dsfile)
+
+    for md in ds._data.values():
+        fn = md['fn']
+        print(fn)
+        try:
+            img = util.load_image(fn)
+        except PIL.UnidentifiedImageError as e:
+            print(f"WARN: skipping {fn} because {e}")
+            continue
+
+        cksum = md5(fn)
+        if cksum != md['md5']:
+            print(' md5 changed!')
+
+        md['md5'] = cksum
+        md['fsz'] = os.path.getsize(fn)
+        md['orig_w'] = img.width
+        md['orig_h'] = img.height
+        pad_to_square(md)
+
+    ds.compact()
+
+
+if __name__ == "__main__":
+    main()
